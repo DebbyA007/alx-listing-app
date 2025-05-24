a0acbf074e2a311dfaafc8039ec1f099bd2743fb
Deborah Akinbola <akinboladeborah400@gmail.com>
2025-05-24 17:01:34 +0100

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+// This is the homepage of the ALX Listing App
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the ALX Listing App</h1>
+      <p>This is a mini Airbnb clone. Start exploring!</p>
+    </div>
+  );
+}
